3eefbf390143b10f71ce262da40128eac949b4a3
konradrogala <konrad.rogala@windowslive.com>
2024-09-28 13:35:25 +0200

    Organizer actions organized

--- organizer_actions.rb
+++ organizer_actions.rb
@@ -13,57 +13,71 @@ class OrganizerActions
       puts "Back(B)"
   
       user_input = gets.chomp
-  
-      if user_input == "1"
-        puts "----------------"
-        puts "Enter event name:"
 
-        event_name = gets.chomp
-
-        puts "----------------"
-        puts "Enter organizer name:"
-
-        organizer_name = gets.chomp
-
-        event = Event.new(organizer: organizer_name, name: event_name)
-        @existed_events << event
-
-        puts "Event created: #{event.name} by #{event.organizer}"
-      elsif user_input == "2"
-        puts "----------------"
-
-        @existed_events.each_with_index do |event, index|
-          puts "(#{index + 1}) #{event.name} by #{event.organizer}" 
-        end
-      elsif user_input == "3"
-        puts "----------------"
-
-        @existed_events.each_with_index do |event, index|
-          puts "(#{index + 1}) #{event.name} by #{event.organizer}" 
-        end
-
-        puts "Enter event number (1-#{@existed_events.size}) to show participants"
-
-        user_input = gets.chomp
-
-        if user_input.to_i.between?(1, @existed_events.size)
-          puts "----------------"
-          puts "Participants of event: #{@existed_events[user_input.to_i - 1]}"
-
-          @existed_events[user_input.to_i - 1].participiants.each_with_index do |participant, index|
-            puts "(#{index + 1}) #{participant}"
-          end
-        else
-          puts "Event not found"
-        end
-
-      elsif user_input == "B"
-        break
-      else
-        next
+      case user_input
+      when "1" then create_event
+      when "2" then show_all_events
+      when "3" then show_participants
+      when "B" then break
+      else next
       end
     end
 
     @existed_events
+  end
+
+  private
+
+  def create_event
+    puts "----------------"
+    puts "Enter event name:"
+
+    event_name = gets.chomp
+
+    puts "----------------"
+    puts "Enter organizer name:"
+
+    organizer_name = gets.chomp
+
+    event = Event.new(organizer: organizer_name, name: event_name)
+    @existed_events << event
+
+    puts "Event created: #{event.name} by #{event.organizer}"
+  end
+
+  def show_all_events
+    puts "----------------"
+
+    if @existed_events.empty? 
+      puts "No events"
+    else
+      @existed_events.each_with_index do |event, index|
+        puts "(#{index + 1}) #{event.name} by #{event.organizer}" 
+      end
+    end
+  end
+
+  def show_participants
+    if @existed_events.empty?
+      puts "----------------"
+      puts "No events"
+    else
+      show_all_events
+
+      puts "Enter event number (1-#{@existed_events.size}) to show participants"
+
+      user_input = gets.chomp
+
+      if user_input.to_i.between?(1, @existed_events.size)
+        puts "----------------"
+        puts "Participants of event: #{@existed_events[user_input.to_i - 1].name}"
+
+        @existed_events[user_input.to_i - 1].participiants.each_with_index do |participant, index|
+          puts "(#{index + 1}) #{participant}"
+        end
+      else
+        puts "Event not found"
+      end
+    end
   end
 end
