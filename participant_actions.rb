8e8f1c9d1ce2e13ecfb0947a28bb1c5ec9c623e9
konradrogala <konrad.rogala@windowslive.com>
2024-09-29 11:14:51 +0200

    show my event as participant

--- participant_actions.rb
+++ participant_actions.rb
@@ -10,14 +10,16 @@ class ParticipantActions
       puts '----------------'
       puts 'What do you want to do?'
       puts 'Show all events(1)'
-      puts 'Register to event(2)'
+      puts 'Show my events(2)'
+      puts 'Register to event(3)'
       puts 'Back(B)'
 
       user_input = gets.chomp
 
       case user_input
       when '1' then show_all_events
-      when '2' then register_to_event
+      when '2' then show_my_events
+      when '3' then register_to_event
       when 'B' then break
       else next
       end
@@ -36,6 +38,32 @@ class ParticipantActions
     else
       @existed_events.each_with_index do |event, index|
         puts "(#{index + 1}) #{event.name} by #{event.organizer}"
+      end
+    end
+  end
+
+  def show_my_events
+    puts '----------------'      
+
+    if @existed_events.empty?
+      puts 'No events'
+    else
+      puts 'Enter your name'
+
+      participant_name = gets.chomp
+
+      participant_events = @existed_events.select do |event|
+        event.participiants.include?(participant_name)
+      end
+
+      if participant_events.empty?
+        puts '----------------'
+        puts 'You dont have any events'
+      else
+        puts '----------------'
+        participant_events.each_with_index do |event, index|
+          puts "(#{index + 1}) #{event.name} by #{event.organizer}"
+        end
       end
     end
   end
